aaaa2a85ceebf2ed23b5e353508b6b3c0a135101
musto <musti.sar4@gmail.com>
2023-02-01 17:24:28 +0100

    Update the Material Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,17 +10,10 @@ import { ProfileComponent } from "./views/users/profile/profile.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { MatInputModule } from "@angular/material/input";
-import { MatButtonModule } from "@angular/material/button";
-import { MatCardModule } from "@angular/material/card";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatSnackBarModule } from "@angular/material/snack-bar";
-import { MatIconModule } from "@angular/material/icon";
-
 import { RegistrationComponent } from "./views/users/registration/registration.component";
 import { MovieCardComponent } from "./views/movies/movie-card/movie-card.component";
 import { WelcomePageComponent } from "./views/welcome-page/welcome-page.component";
+import { AppMaterialModule } from "./app-material.module";
 
 @NgModule({
   declarations: [
@@ -38,13 +31,7 @@ import { WelcomePageComponent } from "./views/welcome-page/welcome-page.componen
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatDialogModule,
-    MatSnackBarModule,
-    MatIconModule,
+    AppMaterialModule,
   ],
   providers: [ApiService],
   bootstrap: [AppComponent],
