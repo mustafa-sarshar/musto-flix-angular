2a9b1e04e272e8e04da9258f8ae6b6eeee24ee9a
musto <musti.sar4@gmail.com>
2023-02-01 09:46:53 +0100

    Update the configs

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,5 +1,5 @@
 const BACKEND_SERVER_URL: string =
-  // "https://ec2-52-87-164-181.compute-1.amazonaws.com:443";
-  "http://localhost:8080";
+  "http://ec2-52-87-164-181.compute-1.amazonaws.com:443";
+  // "http://localhost:8080";
 
 export { BACKEND_SERVER_URL };
