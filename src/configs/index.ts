0555fdc5e476f22e65e8362b440f5fe6ab19b527
musto <musti.sar4@gmail.com>
2023-02-03 11:51:54 +0100

    Update

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,11 +1,5 @@
-import { Routes } from "@angular/router";
-
-import { WelcomePageComponent } from "src/app/views/welcome-page/welcome-page.component";
-import { MovieCardComponent } from "src/app/views/movies/movie-card/movie-card.component";
-import { ProfileComponent } from "src/app/views/users/profile/profile.component";
-
 const BACKEND_SERVER_URL: string =
-  "http://ec2-52-87-164-181.compute-1.amazonaws.com:443";
-// "http://localhost:8080";
+  "https://ec2-52-87-164-181.compute-1.amazonaws.com:443";
+// "https://localhost:8080";
 
 export { BACKEND_SERVER_URL };
