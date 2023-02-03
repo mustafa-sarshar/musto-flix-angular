f703c80c1b0f075e3f61750606bd28dbb6acf3ed
musto <musti.sar4@gmail.com>
2023-02-03 12:50:55 +0100

    Update server url

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,6 +1,6 @@
 const BACKEND_SERVER_URL: string =
-  // "https://ec2-52-87-164-181.compute-1.amazonaws.com:443";
-  "http://ec2-52-87-164-181.compute-1.amazonaws.com:443";
+  "https://ec2-52-87-164-181.compute-1.amazonaws.com:443";
+  // "http://ec2-52-87-164-181.compute-1.amazonaws.com:443";
 // "https://localhost:8080";
 
 export { BACKEND_SERVER_URL };
