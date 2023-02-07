5942b7d3a22b420f959e224ba3439b9976d1daf3
musto <musti.sar4@gmail.com>
2023-02-07 15:09:55 +0100

    Update the server address

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -3,6 +3,6 @@ const BACKEND_SERVER_URL: string =
   // "http://ec2-52-87-164-181.compute-1.amazonaws.com:8080";
   // "https://localhost:4443";
   // "http://localhost:8080";
-  "musto-movie-api-vf0k.onrender.com";
+  "https://musto-movie-api-vf0k.onrender.com";
 
-https: export { BACKEND_SERVER_URL };
+export { BACKEND_SERVER_URL };
