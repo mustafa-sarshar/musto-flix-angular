249542dfef2306efd5dcd0e0053af1814625d114
musto <musti.sar4@gmail.com>
2023-02-14 22:47:16 +0100

    Fix Bugs

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -27,7 +27,7 @@ export class AuthGuard implements CanActivate {
       if (authenticated) {
         return true;
       } else {
-        this.router.navigate(["/"]);
+        this.router.navigate(["/welcome"]);
         return false;
       }
     });
